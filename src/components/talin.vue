90e928b411ab7de37503defd92903eb8c8d1dee4
Rogerz-sh <roger.zhang.sh@foxmail.com>
2019-08-21 21:01:00 +0800

    add file

---
+++ src/components/talin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="animated flipInY">
+    <h1>My name is Talin</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'talin',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {display: inline-block; width: 500px;}
+</style>
